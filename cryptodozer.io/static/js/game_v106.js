58f0465ffdb6af286fb0fecbb94ca98b4c46214e
semsem-ethlas <102934232+semsem-ethlas@users.noreply.github.com>
2022-08-22 23:09:10 +0200

    1
    1

--- cryptodozer.io/static/js/game_v106.js
+++ cryptodozer.io/static/js/game_v106.js
@@ -1,40 +1,40 @@
 (function() {
-    function ready(callback) {
-        if (
-            document.readyState === "complete" ||
-            (document.readyState !== "loading" && !document.documentElement.doScroll)
-        ) {
-            callback();
-        } else {
-            document.addEventListener("DOMContentLoaded", callback);
-        }
+  function ready(callback) {
+    if (
+      document.readyState === "complete" ||
+      (document.readyState !== "loading" && !document.documentElement.doScroll)
+    ) {
+      callback();
+    } else {
+      document.addEventListener("DOMContentLoaded", callback);
     }
+  }
 
-    function addScript(src) {
-        var el = document.createElement('script');
+  function addScript(src) {
+    var el = document.createElement('script');
 
-        el.setAttribute('src', src);
-        document.body.appendChild(el);
-    }
+    el.setAttribute('src', src);
+    document.body.appendChild(el);
+  }
 
-    function getCurrentScript() {
-        var scripts = document.getElementsByTagName('script');
-        var index = scripts.length - 1;
+  function getCurrentScript() {
+    var scripts = document.getElementsByTagName('script');
+    var index = scripts.length - 1;
 
-        return scripts[index];
-    }
+    return scripts[index];
+  }
 
-    var gameScript = getCurrentScript();
-    var pathParts = gameScript.src.split('/');
-    var scriptDir;
-    var hash = parseInt(Math.random() * 1000000);
-    var scriptPath;
+  var gameScript = getCurrentScript();
+  var pathParts = gameScript.src.split('/');
+  var scriptDir;
+  var hash = parseInt(Math.random() * 1000000);
+  var scriptPath;
 
-    pathParts.pop();
-    scriptDir = pathParts.join('/');
-    scriptPath = scriptDir + '/runtime.js?h=' + hash;
+  pathParts.pop();
+  scriptDir = pathParts.join('/');
+  scriptPath = scriptDir + '/runtime.js?h=' + hash;
 
-    ready(function() {
-        addScript(scriptPath);
-    });
+  ready(function() {
+    addScript(scriptPath);
+  });
 })();
